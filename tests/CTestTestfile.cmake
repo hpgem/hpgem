c7d6555de20dd3805d3fab4933a7a150d1c0f37d
Anthony Thornton <a.r.thornton@utwente.nl>
2013-03-15 17:24:40 +0100

    r166 ...and some more.

--- tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/antRthorn/Dropbox/hpgem/Phoenix/tests
-# Build directory: /Users/antRthorn/Dropbox/hpgem/Phoenix/tests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(unit)
-SUBDIRS(files)
-SUBDIRS(problems)
