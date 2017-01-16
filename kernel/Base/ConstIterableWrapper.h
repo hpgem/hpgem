aa3af481c0ed10f03404a28743cafb5701746eb6
Freekjan Brink <f.brink@utwente.nl>
2017-01-16 11:45:01 +0100

    r912 bug-fix: ConstIterableWrapper now correctly propagates from const pointer to const pointer to const

--- kernel/Base/ConstIterableWrapper.h
+++ kernel/Base/ConstIterableWrapper.h
@@ -24,6 +24,9 @@
 
 #include <vector>
 
+template<typename T>
+class IteratorOverConst;
+
 /**
  * small wrapper to iterate over vectors of pointers where you shouldn't modify whatever is in the pointer
  */
@@ -35,17 +38,145 @@ public:
     {
     }
 
-    typename std::vector<const T*>::const_iterator begin() const
+    IteratorOverConst<typename std::vector<T*>::const_iterator> begin() const
     {
         return data.begin();
     }
 
-    typename std::vector<const T*>::const_iterator end() const
+    IteratorOverConst<typename std::vector<T*>::const_iterator> end() const
     {
         return data.end();
     }
 private:
     const std::vector<T*>& data;
 };
+
+template<template<typename, typename...> class Iterator, typename T, typename... Q>
+class IteratorOverConst<Iterator<T*const*, Q...>>{
+public:
+    using difference_type = typename std::iterator_traits<Iterator<T*const*, Q...>>::difference_type;
+    using value_type = typename std::iterator_traits<Iterator<T*const*, Q...>>::value_type;
+    using pointer = const T*const*;//pointer to const pointer to const T
+    using reference = const T*const;//const pointer to const T (no need to keep reference, since pointers are cheap to copy, also the implicit conversion from T*const to const T*const would cause the reference to be a reference to the wrong thing)
+    using iterator_category = typename std::iterator_traits<Iterator<T*const*, Q...>>::iterator_category;
+
+    //! default constructor
+    IteratorOverConst() = default;
+
+    //! Copy constructor
+    IteratorOverConst(const IteratorOverConst& i) = default;
+    //! Move constructor
+
+    IteratorOverConst(IteratorOverConst&& i) = default;
+
+    //! actual constructor;
+    IteratorOverConst(Iterator<T*const*, Q...> nested) : inner(nested) {}
+
+    //! Copy Assignment operator
+    IteratorOverConst& operator=(const IteratorOverConst& i) = default;
+
+    //! Move Assignment operator
+    IteratorOverConst& operator=(IteratorOverConst&& i) = default;
+
+    reference operator*() const
+    {
+        return *inner;
+    }
+
+    pointer operator->() const
+    {
+        return &(*inner);
+    }
+
+    IteratorOverConst& operator++()
+    {
+        ++inner;
+        return *this;
+    }
+
+    IteratorOverConst& operator--()
+    {
+        --inner;
+        return *this;
+    }
+
+    IteratorOverConst operator++(int) // postinc
+    {
+        IteratorOverConst result = *this;
+        ++inner;
+        return result;
+    }
+
+    IteratorOverConst operator--(int) // postdec
+    {
+        IteratorOverConst result = *this;
+        --inner;
+        return result;
+    }
+
+    //! Are they the same iterators? 
+    bool operator==(const IteratorOverConst& other) const
+    {
+        inner == other.inner;
+    }
+
+    //! Are they different iterators?
+    bool operator!=(const IteratorOverConst &i) const
+    {
+        return inner != i.inner;
+    }
+
+    IteratorOverConst& operator+=(difference_type n) {
+        inner += n;
+        return *this;
+    }
+
+    IteratorOverConst operator+(difference_type n) const {
+        IteratorOverConst result{*this};
+        return result+=n;
+    }
+
+    IteratorOverConst& operator -=(difference_type n) {
+        inner-=n;
+        return *this;
+    }
+
+    IteratorOverConst operator-(difference_type n) const {
+        IteratorOverConst result{*this};
+        return result -=n;
+    }
+
+    difference_type operator-(const IteratorOverConst& other) const{
+        return inner - other.inner;
+    }
+
+    reference operator[](difference_type n) const {
+        return *(*this + n);
+    }
+
+    bool operator<(const IteratorOverConst& other) const {
+        return inner < other.inner;
+    }
+
+    bool operator>(const IteratorOverConst& other) const {
+        return inner > other.inner;
+    }
+
+    bool operator<=(const IteratorOverConst& other) const {
+        return inner <= other.inner;
+    }
+
+    bool operator>=(const IteratorOverConst& other) const {
+        return inner >= other.inner;
+    }
+private:
+    Iterator<T*const*, Q...> inner;
+};
+
+template <typename T>
+IteratorOverConst<T> operator+(typename IteratorOverConst<T>::difference_type n, const IteratorOverConst<T>& a) {
+    IteratorOverConst<T> result{a};
+    return result+=n;
+}
 
 #endif //HPGEM_CONSTITERABLEWRAPPER_H
