32f9ab45ae896641058a4794eae0c8c7b0ff29f7
Lars Corbijn van Willenswaard <l.j.corbijnvanwillenswaard@utwente.nl>
2019-01-06 15:50:00 +0100

    r954 Add the forgotten ReferenceSimplex itself
    Forgot to commit this due to the difference between git & svn, again.

---
+++ kernel/Geometry/ReferenceSimplex.h
@@ -0,0 +1,78 @@
+#ifndef HPGEM_REFERENCESIMPLEX_H
+#define HPGEM_REFERENCESIMPLEX_H
+
+#include "../Logger.h"
+
+#include "ReferenceGeometry.h"
+
+namespace LinearAlgebra
+{
+    template<std::size_t DIM>
+    class SmallVector;
+}
+
+namespace Geometry
+{
+    /// Implementation of a reference simplex (triangle, tetrahedron)
+    ///
+    /// The standard reference simplex is formed by the origin and each of the
+    /// unit vectors, numbered in increasing order of the coordinate (e.g. O,X,Y)
+    ///
+    /// Note that the reference line is based on the hypercube.
+    /// \tparam DIM The dimension of the simplex.
+    template<std::size_t DIM>
+    class ReferenceSimplex : public ReferenceGeometry
+    {
+    public:
+        ReferenceSimplex(const ReferenceGeometryType& geoT, std::string name);
+
+        /// Compute the bary centric coordinates of a point
+        /// \param p The point
+        /// \return The barycentric coordinates
+        LinearAlgebra::SmallVector<DIM+1> baryCentricCoordinates(const PointReference<DIM>& p) const;
+
+        template<std::size_t DIM1>
+        friend std::ostream& operator<<(std::ostream& os, const ReferenceSimplex<DIM1>& simplex);
+
+        const PointReferenceBase& getCenter() const final
+        {
+            return center_;
+        }
+
+        std::size_t getNumberOfNodes() const final
+        {
+            return DIM + 1;
+        }
+
+        const PointReferenceBase& getReferenceNodeCoordinate(const std::size_t& localIndex) const final
+        {
+            logger.assert_debug(localIndex >= 0 && localIndex < getNumberOfNodes(),
+                    "Local index % should be in the range [0, %]", localIndex, getNumberOfNodes());
+            return points_[localIndex];
+        }
+
+        const PointReference<DIM>& getPoints()
+        {
+            return points_;
+        }
+
+    private:
+        const PointReference<DIM> center_;
+        // Leave non const to allow actual initialization
+        PointReference<DIM> points_[DIM+1];
+    };
+
+    template<std::size_t DIM>
+    std::ostream& operator<<(std::ostream& os, const ReferenceSimplex<DIM>& simplex)
+    {
+        os << simplex.name_ << " =( ";
+        for(std::size_t i = 0; i < simplex.getNumberOfNodes(); ++i)
+        {
+            os << simplex.points_[i] << "\t";
+        }
+        os << ")" << std::endl;
+        return os;
+    }
+}
+
+#endif //HPGEM_REFERENCESIMPLEX_H
---
+++ kernel/Geometry/ReferenceSimplex_Impl.h
@@ -0,0 +1,53 @@
+#ifndef HPGEM_REFERENCESIMPLEX_IMPL_H
+#define HPGEM_REFERENCESIMPLEX_IMPL_H
+
+#include "ReferenceSimplex.h"
+#include "PointReference.h"
+
+#include "../LinearAlgebra/SmallVector.h"
+
+
+namespace Geometry
+{
+    // Helper functions for the constructor
+    template<std::size_t DIM>
+    LinearAlgebra::SmallVector<DIM> createCenter()
+    {
+        LinearAlgebra::SmallVector<DIM> result;
+        result.set(1.0/(DIM+1));
+        return result;
+    }
+
+    template<std::size_t DIM>
+    ReferenceSimplex<DIM>::ReferenceSimplex(const ReferenceGeometryType& geoT, std::string name)
+        : ReferenceGeometry(geoT, name)
+        , center_ (createCenter<DIM>())
+    {
+        LinearAlgebra::SmallVector<DIM> zero;
+        zero.set(0);
+        for(std::size_t i = 0; i <= DIM; ++i)
+        {
+            this->points_[i].setCoordinates(zero);
+            if(i > 0)
+                this->points_[i].setCoordinate(i-1, 1);
+        }
+    }
+
+    template<std::size_t DIM>
+    LinearAlgebra::SmallVector<DIM+1> ReferenceSimplex<DIM>::baryCentricCoordinates(
+            const Geometry::PointReference<DIM> &p) const
+    {
+        LinearAlgebra::SmallVector<DIM+1> result;
+        result[0] = 1;
+        for(std::size_t i = 0; i < DIM; ++i)
+        {
+            result[0] -= p[i];
+            result[i+1] = p[i];
+        }
+        return result;
+    }
+
+
+}
+
+#endif //HPGEM_REFERENCESIMPLEX_IMPL_H
