7c81330cf682d0b5a9637e85ef97c4e20966d202
jwehner <jenswehner@gmail.com>
2020-04-10 14:26:40 +0200

    changed use of TARGETS for BLAS LAPACK and MPI

--- conf/cmake/ConfigureDependencies.cmake
+++ conf/cmake/ConfigureDependencies.cmake
@@ -3,26 +3,17 @@
 #############################
 
 FIND_PACKAGE(BLAS REQUIRED)
-if(NOT BLAS_FOUND)
-    message(SYSTEM "Could not find external BLAS, using reference implementation instead")
-    set(hpGEM_USE_EXTERNAL_BLAS OFF CACHE BOOL "Try to use a version of BLAS optimised for your system" FORCE)
-else()
-    # Linking target
+if(BLAS_FOUND AND NOT TARGET BLAS::BLAS)
+    # CREATING A BLAS::BLAS TARGET as FINDBLAS does not create one
     add_library(BLAS::BLAS INTERFACE IMPORTED)
-    target_link_libraries(BLAS::BLAS INTERFACE "${BLAS_LIBRARIES}")
-    target_link_options(BLAS::BLAS INTERFACE "${BLAS_LINKER_FLAGS}")
+    set_target_properties(BLAS::BLAS PROPERTIES INTERFACE_LINK_LIBRARIES "${BLAS_LIBRARIES}" INTERFACE_LINK_FLAGS "${BLAS_LINKER_FLAGS}" )
 endif()
 
-
 FIND_PACKAGE(LAPACK REQUIRED)
-if(NOT LAPACK_FOUND)
-    message(SYSTEM "Could not find external LAPACK, using reference implementation instead")
-    set(hpGEM_USE_EXTERNAL_LAPACK OFF CACHE BOOL "Try to use a version of LAPACK optimised for your system" FORCE)
-else()
-    # Linking target
+if(LAPACK_FOUND AND NOT TARGET LAPACK::LAPACK)
+    # CREATING A LAPACK::LAPACK TARGET as FINDLAPACK does not create one
     add_library(LAPACK::LAPACK INTERFACE IMPORTED)
-    target_link_libraries(LAPACK::LAPACK INTERFACE "${LAPACK_LIBRARIES}")
-    target_link_options(LAPACK::LAPACK INTERFACE "${LAPACK_LINKER_FLAGS}")
+    set_target_properties(BLAS::BLAS PROPERTIES INTERFACE_LINK_LIBRARIES  "${LAPACK_LIBRARIES}" INTERFACE_LINK_FLAGS "${LAPACK_LINKER_FLAGS}" )
 endif()
 
 ### OPTIONAL DEPENDENCIES ###
@@ -32,18 +23,15 @@ if(hpGEM_USE_MPI)
     FIND_PACKAGE(MPI REQUIRED)
     # Add OMPI_SKIP_MPICXX to exclude the deprecated c++ bindings of MPI
     add_definitions(-DHPGEM_USE_MPI -DOMPI_SKIP_MPICXX)
-    include_directories(${MPI_CXX_INCLUDE_PATH})
-    if(NOT MPI_FOUND)
-        message(FATAL_ERROR "The option you have chosen requires mpi and you do not have this installed. Please install")
-    endif()
     # Create target for easy linking
     if(NOT TARGET MPI::MPI_CXX)
         # Note, from CMAKE 3.9 there is a proper target defined
         add_library(MPI::MPI_CXX INTERFACE IMPORTED)
-        target_include_directories(MPI::MPI_CXX INTERFACE ${MPI_CXX_INCLUDE_PATH})
-        target_link_libraries(MPI::MPI_CXX INTERFACE "${MPI_CXX_LIBRARIES}")
-        target_compile_options(MPI::MPI_CXX INTERFACE "${MPI_CXX_COMPILE_FLAGS}")
-        target_link_options(MPI::MPI_CXX INTERFACE "${MPI_CXX_LINK_FLAGS}")
+        set_target_properties(MPI::MPI_CXX PROPERTIES 
+        INTERFACE_LINK_LIBRARIES "${MPI_CXX_LIBRARIES}" 
+        INTERFACE_COMPILE_OPTIONS "${MPI_CXX_COMPILE_FLAGS}"
+        INTERFACE_LINK_OPTIONS "${MPI_CXX_LINK_FLAGS}" 
+        INTERFACE_INCLUDE_DIRECTORIES "${MPI_CXX_INCLUDE_PATH}")
     endif()
 endif()
 
@@ -51,9 +39,6 @@ if(hpGEM_USE_METIS)
     FIND_PACKAGE(METIS REQUIRED)
     add_definitions(-DHPGEM_USE_METIS)
     include_directories(${METIS_INCLUDE_DIR})
-    if(NOT METIS_FOUND)
-        message(FATAL_ERROR "The option you have chosen requires metis and you do not have this installed. Please install")
-    endif()
     # Create target for easy linking
     add_library(METIS::METIS INTERFACE IMPORTED)
     target_include_directories(METIS::METIS INTERFACE "${METIS_INCLUDE_DIR}")
@@ -63,9 +48,6 @@ endif()
 if(hpGEM_USE_QHULL)
     FIND_PACKAGE(QHULL REQUIRED)
     add_definitions(-DHPGEM_USE_QHULL)
-    if(NOT QHULL_FOUND)
-        message(FATAL_ERROR "The option you have chosen requires QHull and you do not have this installed. Please install")
-    endif()
     # Create target for easy linking
     add_library(QHULL::QHULL INTERFACE IMPORTED)
     target_include_directories(QHULL::QHULL INTERFACE "${QHULL_INCLUDE_DIR}")
