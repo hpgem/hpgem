b4bbe5fa4b3db2b9fd4b7db069554b40c5306d1b
jenswehner <jenswehner@gmail.com>
2021-02-18 14:39:05 +0100

    working on nodes

--- applications/Preprocessor/lib/gmsh.cpp
+++ applications/Preprocessor/lib/gmsh.cpp
@@ -46,14 +46,6 @@ using namespace hpgem;
 
 namespace Preprocessor {
 
-GmshReader::GmshReader(std::string filename) {
-
-    Filehandle_.open(filename);
-    logger.assert_always(Filehandle_.is_open(), "Cannot open msh meshfile.");
-    logger.assert_always(Filehandle_.good(),
-                         "Something is not so good about this mesh");
-}
-
 //
 // Non-class helper function for line-reading
 // Because getline works only on text files from the OS that it was compiled
@@ -91,8 +83,9 @@ std::istream& safeGetline(std::istream& is, std::string& t) {
     }
 }
 
-bool GmshReader::locate_in_file(std::ifstream& filestream,
-                                const std::string& searchname) {
+// find a searchname in the MSH file and return the file stream after that
+// line
+bool locate_in_file(std::ifstream& filestream, const std::string& searchname) {
     // to find a specific keyword in the MSH file and return the file stream
     std::string temp;
     bool found = false;
@@ -106,6 +99,92 @@ bool GmshReader::locate_in_file(std::ifstream& filestream,
     }
     logger.assert_always(found, "The " + searchname + "field was not found");
     return found;
+}
+
+GmshReader::GmshReader(std::string filename) {
+    // Fileformat is defined in
+    // https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format
+    Filehandle_.open(filename);
+    logger.assert_always(Filehandle_.is_open(), "Cannot open msh meshfile.");
+    logger.assert_always(Filehandle_.good(),
+                         "Something is not so good about this mesh");
+
+    bool found_header = locate_in_file(Filehandle_, "$MeshFormat");
+    if (!found_header) {
+        logger.assert_always(found_header, "Header of gmsh file % not found",
+                             filename);
+    } else {
+        double version;
+        int filetype, datasize;
+        Filehandle_ >> version >> filetype >> datasize;
+
+        logger(VERBOSE, "msh version is % and storage format is %", version,
+               (filetype == 1 ? "binary" : "ASCII"));
+        logger.assert_always(datasize == sizeof(size_t),
+                             "Datasize is % vs sizeof(size_t) is %", datasize,
+                             sizeof(size_t));
+        if (filetype == 1) {
+            int endianness;
+            Filehandle_ >> endianness;
+            if (endianness == 1) {
+                logger(VERBOSE, "endianness of machine and file agree");
+            }
+        }
+        logger.assert_always(version >= 4.1,
+                             "Error: Cannot read file versions less than 4.1");
+        logger.assert_always(filetype == 0,
+                             "Error: Cannot read binary format files");
+    }
+    bool found_nodes = locate_in_file(Filehandle_, "$Nodes");
+    if (!found_nodes) {
+        logger.assert_always(
+            found_header, "Nodes section of gmsh file %  not found", filename);
+    } else {
+        size_t nodes_total_entities;
+        size_t raw_N_nodes;
+        size_t nodes_min_index;
+        size_t nodes_max_index;
+        Filehandle_ >> nodes_total_entities >> raw_N_nodes >> nodes_min_index >>
+            nodes_max_index;
+
+        bool is_continuous_data =
+            (raw_N_nodes == (nodes_max_index - nodes_min_index + 1));
+        logger.assert_always(is_continuous_data,
+                             "Error: Cannot read non continous nodes");
+        nodes_.reserve(raw_N_nodes);
+        for (size_t block_index = 0; block_index < nodes_total_entities;
+             block_index++) {
+            int entityDim;
+            int entityTag;
+            int parametric;
+            size_t numNodesInBlock;
+            Filehandle_ >> entityDim >> entityTag >> parametric >>
+                numNodesInBlock;
+            std::cout << entityDim << " " << entityTag << " " << parametric
+                      << " " << numNodesInBlock << std::endl;
+
+            logger.assert_always(parametric == 0,
+                                 "Error: Cannot read parametric coordinates");
+
+            for (size_t nodeindex = 0; nodeindex < numNodesInBlock;
+                 nodeindex++) {
+                size_t nodeTag;
+                Filehandle_ >> nodeTag;
+            }
+            for (size_t nodeindex = 0; nodeindex < numNodesInBlock;
+                 nodeindex++) {
+                double x;
+                double y;
+                double z;
+                Filehandle_ >> x >> y >> z;
+                MeshSource::Node temp;
+                temp.coordinates.push_back(std::vector<double>{x, y, z});
+                nodes_.push_back(temp);
+            }
+        }
+    }
+
+    bool found_elements = locate_in_file(Filehandle_, "$Elements");
 }
 
 Range<MeshSource::Node> GmshReader::getNodeCoordinates() { ; }
--- applications/Preprocessor/lib/gmsh.h
+++ applications/Preprocessor/lib/gmsh.h
@@ -46,7 +46,8 @@
 #include <string>
 #include <map>
 
-// Most of this was taken from https://github.com/Applied-Scientific-Research/gmsh-reader/tree/main/src
+// Most of this was taken from
+// https://github.com/Applied-Scientific-Research/gmsh-reader/tree/main/src
 
 using namespace hpgem;
 
@@ -59,23 +60,15 @@ class GmshReader : public MeshSource {
     Range<MeshSource::Node> getNodeCoordinates() final;
     Range<MeshSource::Element> getElements() final;
 
-    std::size_t getDimension() const final { return dimension_; }
+    std::size_t getDimension() const final { return 3; }
 
    private:
     std::ifstream Filehandle_;
 
-    std::size_t dimension_;
+    std::vector<MeshSource::Node> nodes_;
 
-    uint32_t N_nodes = 0;     //# of nodes read from msh file
-	uint32_t N_elements = 0;  //# of elements read from msh file
-	std::vector<node> nodes;  // coordinates of the nodes
-	std::vector<edge> edges;	 // types and nodes constituting edges of elements
-	std::vector<element2d> elements; // types and nodes constituting elements
-	std::vector<boundary> boundaries; // the boundaries of the problem, including the names and the edges that form each boundary
+    std::vector<MeshSource::Element> elements_;
 
-
-    // find a searchname in the MSH file and return the file stream after that line
-	bool locate_in_file(std::ifstream& filestream, const std::string& searchname);
 };
 }  // namespace Preprocessor
 
