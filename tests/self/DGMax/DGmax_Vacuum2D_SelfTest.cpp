9e3921ca49c76fddcf21e6a692e37ddb505caf42
Lars Corbijn van Willenswaard <l.j.corbijnvanwillenswaard@utwente.nl>
2021-03-09 16:01:35 +0100

    Fix the projection of initial vectors
    This broke with 08c1ceb43aa1b1f247537c60ddbf567c024783b. Due to not
    testing different configurations of DGMaxEigenvalue this was not caught
    earlier.

--- tests/self/DGMax/DGmax_Vacuum2D_SelfTest.cpp
+++ tests/self/DGMax/DGmax_Vacuum2D_SelfTest.cpp
@@ -66,6 +66,14 @@ DGMax::EVConvergenceResult expected ({
      {1.204162236,5.441822184,5.556058059,7.139002763,7.226395987,7.684761581,8.897550726}
 
  });
+
+DGMax::EVConvergenceResult expected2({
+    {1.2043368890,5.4110861512,5.5242942569,7.0664810486,7.1494436437,7.7278002899,8.9463017274,9.0890054437,10.1789455152,11.3362127914,11.4352596910},
+    {1.2042038880,5.4345612104,5.5485595891,7.1219632000,7.2083141835,7.6957944040,8.9107366441,9.0516696919,10.1143353023,11.6076760680},
+    {1.2041705703,5.4403727357,5.5545614554,7.1356054603,7.2227907973,7.6870050696,8.9002601508,9.0406100487,10.0948497388,11.6724130102},
+    {1.2041622356,5.4418221838,5.5560580595,7.1390027626,7.2263959873,7.6847615808,8.8975507256,9.0377472104,10.0897957246,11.6884390538}
+});
+
 // clang-format on
 
 int main(int argc, char** argv) {
@@ -93,6 +101,16 @@ int main(int argc, char** argv) {
                                               config, &expected);
     DGMax::EVConvergenceResult result = testCase.run(runAsTest);
 
+    // Second test of the same algorithm, but with completely different
+    // settings, in an attempt to cover as many different paths with the least
+    // number of tests.
+    config.shiftFactor_ = -1.0;
+    config.useHermitian_ = true;
+    config.useProjector_ = DGMaxEigenvalueBase::ALL;
+    DGMax::DGMaxEVConvergenceTest<2> testCase2(testPoint, meshes, 1e-8, 1,
+                                               config, &expected);
+    DGMax::EVConvergenceResult result2 = testCase.run(runAsTest);
+
     // Code to check the results if they change
     // Expected convergence speed = 2^2p = 4
     if (!runAsTest) {
@@ -105,6 +123,20 @@ int main(int argc, char** argv) {
             analytical.computeLinearSpectrum(testPoint.getKPoint(), 20);
 
         std::cout << "Raw frequency table" << std::endl;
+        result.printFrequencyTable(linear);
+
+        result.printResultCode(10);
+
+        std::cout << "Cleanup results" << std::endl;
+        result.filterResults(
+            0.01, true);  // Small eigenvalues are to be expected from DGMax
+        result.printFrequencyTable(linear);
+        result.printErrorTable(linear);
+
+        // Some spacing between the results for the two seperate tests
+        std::cout << "\n\n";
+
+        std::cout << "Raw frequency table 2" << std::endl;
         result.printFrequencyTable(linear);
 
         result.printResultCode(10);
