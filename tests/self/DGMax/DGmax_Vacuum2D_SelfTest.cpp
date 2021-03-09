39b6091d8044a6edbb43a7a59bc42c652519bd82
Lars Corbijn van Willenswaard <l.j.corbijnvanwillenswaard@utwente.nl>
2021-03-09 20:58:50 +0100

    Fix commiting mistakes

--- tests/self/DGMax/DGmax_Vacuum2D_SelfTest.cpp
+++ tests/self/DGMax/DGmax_Vacuum2D_SelfTest.cpp
@@ -67,6 +67,13 @@ DGMax::EVConvergenceResult expected ({
 
  });
 
+DGMax::EVConvergenceResult expected2({
+    {1.2043368890,5.4110861512,5.5242942569,7.0664810486,7.1494436437,7.7278002899,8.9463017274,9.0890054437,10.1789455152,11.3362127914,11.4352596910},
+    {1.2042038880,5.4345612104,5.5485595891,7.1219632000,7.2083141835,7.6957944040,8.9107366441,9.0516696919,10.1143353023,11.6076760680},
+    {1.2041705703,5.4403727357,5.5545614554,7.1356054603,7.2227907973,7.6870050696,8.9002601508,9.0406100487,10.0948497388,11.6724130102},
+    {1.2041622356,5.4418221838,5.5560580595,7.1390027626,7.2263959873,7.6847615808,8.8975507256,9.0377472104,10.0897957246,11.6884390538}
+});
+
 // clang-format on
 
 int main(int argc, char** argv) {
@@ -103,7 +110,7 @@ int main(int argc, char** argv) {
     config.useHermitian_ = true;
     config.useProjector_ = DGMaxEigenvalueBase::ALL;
     DGMax::DGMaxEVConvergenceTest<2> testCase2(testPoint, meshes, 1e-8, 1,
-                                               config, &expected);
+                                               config, &expected2);
     DGMax::EVConvergenceResult result2 = testCase.run(runAsTest);
 
     // Code to check the results if they change
@@ -132,14 +139,14 @@ int main(int argc, char** argv) {
         std::cout << "\n\n";
 
         std::cout << "Raw frequency table 2" << std::endl;
-        result.printFrequencyTable(linear);
+        result2.printFrequencyTable(linear);
 
-        result.printResultCode(10);
+        result2.printResultCode(10);
 
         std::cout << "Cleanup results" << std::endl;
-        result.filterResults(
+        result2.filterResults(
             0.01, true);  // Small eigenvalues are to be expected from DGMax
-        result.printFrequencyTable(linear);
-        result.printErrorTable(linear);
+        result2.printFrequencyTable(linear);
+        result2.printErrorTable(linear);
     }
 }
