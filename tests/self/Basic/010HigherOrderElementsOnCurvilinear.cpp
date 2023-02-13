cb68851d450bf2c9a22cbdbc3eec326829093063
Lars Corbijn van Willenswaard <l.j.corbijnvanwillenswaard@utwente.nl>
2023-02-13 21:48:59 +0100

    Add test for assigning conforming elemtents to curvilinear

---
+++ tests/self/Basic/010HigherOrderElementsOnCurvilinear.cpp
@@ -0,0 +1,72 @@
+/*
+This file forms part of hpGEM. This package has been developed over a number of
+years by various people at the University of Twente and a full list of
+contributors can be found at http://hpgem.org/about-the-code/team
+
+This code is distributed using BSD 3-Clause License. A copy of which can found
+below.
+
+
+Copyright (c) 2023, University of Twente
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "Base/MeshManipulator.h"
+#include "Base/ConfigurationData.h"
+
+#include "Base/CommandLineOptions.h"
+#include "Logger.h"
+#include <hpgem-cmake.h>
+
+using namespace hpgem;
+
+int main(int argc, char** argv) {
+    using namespace std::string_literals;
+    Base::parse_options(argc, argv);
+
+    Base::MeshManipulator<2> mesh(new Base::ConfigurationData(2, 0));
+    mesh.readMesh(getCMAKE_hpGEM_SOURCE_DIR() +
+                  "/tests/files/circle-domain-quadratic-N1.hpgem"s);
+    // Set lots of basis functions, but there should be only 2 per topological
+    // node
+    mesh.useDefaultConformingBasisFunctions(4);
+    mesh.useDefaultConformingBasisFunctions(4, 1);
+
+    for (Base::Node* node : mesh.getNodesList()) {
+        bool isTopoNode = node->getTopologicalNodeNumber(0) >= 0;
+        int bf = node->getTotalLocalNumberOfBasisFunctions();
+        if (isTopoNode) {
+            logger.assert_always(
+                bf == 2,
+                "Topological node with incorrect number of basis functions");
+        } else {
+            logger.assert_always(bf == 0,
+                                 "Geometric node with basis functions");
+        }
+    }
+}
